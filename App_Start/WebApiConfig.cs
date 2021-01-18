770e98d474fac2e46403523079af5445864f4fb1
Ahtisham <55557824+AhtishamAli-star@users.noreply.github.com>
2021-01-18 23:26:45 +0500

    Add files via upload
    These are the main code excluding reference

---
+++ App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Net.Http;
+using System.Web.Http.Cors;
+using System.Net.Http.Headers;
+
+namespace WebApplication1
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            //web api json response configuration
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            //wbe api cors enabled
+            config.EnableCors(new EnableCorsAttribute("*", "*", "*"));
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ Controllers/cvMakerController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace WebApplication1.Controllers
+{
+    [RoutePrefix("api/cvMaker")]
+
+    public class cvMakerController : ApiController
+    {
+        SqlConnection sqlcon;
+        SqlCommand cmd;
+        DataTable dt;
+        [HttpPost]
+        [Route("GET_CV_Details")]
+        public HttpResponseMessage GETCVDETAILS(getDetailsModel gdm)
+        {
+            dt = new DataTable();
+            try
+            {
+                using (sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["dbString"].ConnectionString))
+                {
+                    sqlcon.Open();
+                    cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = sqlcon;
+                    cmd.CommandText = "GetRecord";
+                    cmd.Parameters.AddWithValue("@personId",gdm.personId);
+
+                    SqlDataAdapter sqlda = new SqlDataAdapter(cmd);
+                    sqlda.Fill(dt);
+                    
+                    sqlcon.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK,dt);
+                }
+            }
+            catch (Exception ex)
+            {
+                sqlcon.Close();
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+            }
+
+        }
+
+        [HttpPost]
+        [Route("POST_CV_Details")]
+        public HttpResponseMessage POSTCVDETAILS(detailsModel dm)
+        {
+            try
+            {
+                using (sqlcon = new SqlConnection(ConfigurationManager.ConnectionStrings["dbString"].ConnectionString))
+                {
+                    sqlcon.Open();
+                    cmd = new SqlCommand();
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Connection = sqlcon;
+                    cmd.CommandText = "insertRecord";
+                    cmd.Parameters.AddWithValue("@name", dm.name);
+                    cmd.Parameters.AddWithValue("@email", dm.email);
+                    cmd.Parameters.AddWithValue("@phoneNumber", dm.phoneNumber);
+                    cmd.Parameters.AddWithValue("@address", dm.address);
+                    cmd.Parameters.AddWithValue("@about", dm.about);
+                    cmd.Parameters.AddWithValue("@picture", dm.picture);
+                    cmd.Parameters.AddWithValue("@skills", dm.skills);
+                    cmd.Parameters.AddWithValue("@hobbies", dm.hobbies);
+                    cmd.Parameters.AddWithValue("@jobTitle", dm.jobTitle);
+                    cmd.Parameters.AddWithValue("@employer", dm.employer);
+                    cmd.Parameters.AddWithValue("@fromTo", dm.fromTo);
+                    cmd.Parameters.AddWithValue("@description", dm.description);
+                    cmd.Parameters.AddWithValue("@personId", dm.personId);
+
+                    cmd.ExecuteNonQuery();
+                    sqlcon.Close();
+                    return Request.CreateResponse(HttpStatusCode.OK);
+                }
+            }
+            catch (Exception ex)
+            {
+                sqlcon.Close();
+                return Request.CreateResponse(HttpStatusCode.ExpectationFailed);
+            }
+
+
+        }
+
+    }
+}
